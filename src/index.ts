35dd1700bea9a91c75a6f1b8bb4f272305b91658
arjun7736 <arjunvengassery123@gmail.com>
2024-09-29 09:15:17 +0530

    Update :Fixed Trust Proxy Error

--- src/index.ts
+++ src/index.ts
@@ -3,40 +3,38 @@ import dotenv from "dotenv";
 import connectDB from "./infrastructure/database/db";
 import errorMiddleware from "./middlewares/ErrorMiddleware";
 dotenv.config();
-import authRoute from "./interfaces/routes/AuthRoute"
-import adminRoute from "./interfaces/routes/AdminRoute"
-import userRoute from "./interfaces/routes/UserRoute"
-import morgan from "morgan"
+import authRoute from "./interfaces/routes/AuthRoute";
+import adminRoute from "./interfaces/routes/AdminRoute";
+import userRoute from "./interfaces/routes/UserRoute";
+import morgan from "morgan";
 import rateLimit from "express-rate-limit";
 
 const apirateLimit = rateLimit({
-  windowMs:15*60*1000,
-  max:100,
-  standardHeaders:true,
-  legacyHeaders:true,
-  message:"Too many Requests From this Address"
-})
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: "Too many Requests From this Address",
+});
 
 const port: string | undefined = process.env.PORT;
 const app = express();
 
-connectDB()
-app.set('trust proxy', true);
+connectDB();
+app.set("trust proxy", false);
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
-app.use("/api",apirateLimit)
-app.use("/api/auth",authRoute)
-app.use("/api/admin",adminRoute)
-app.use("/api/user",userRoute)
+app.use("/api", apirateLimit);
+app.use("/api/auth", authRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/user", userRoute);
 
-app.use(errorMiddleware)
-
+app.use(errorMiddleware);
 
 if (port) {
   app.listen(port, () => {
     console.log("server is running");
   });
 }
-
