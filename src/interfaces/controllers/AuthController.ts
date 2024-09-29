38a71a90b4ca1b116e09242117c5ba50033537ea
arjun7736 <arjunvengassery123@gmail.com>
2024-09-29 09:32:44 +0530

    test

--- src/interfaces/controllers/AuthController.ts
+++ src/interfaces/controllers/AuthController.ts
@@ -14,6 +14,7 @@ export class AuthController {
   async login(req: Request, res: Response, next: NextFunction) {
     try {
       const { email, password } = req.body;
+      console.log(req.body)
       const login = await this.userLogin.execute(email, password);
       const token = generateToken(login._id);
       res.json({ user: login, token: token });
